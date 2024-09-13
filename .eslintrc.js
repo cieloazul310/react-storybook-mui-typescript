f34f08d5c132ff62a14233bf1149d04171864e52
cieloazul310 <cieloazul310@gmail.com>
2024-09-13 16:43:24 +0900

    chore(deps): upgrade storybook to v8

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,13 +4,7 @@ module.exports = {
     es2021: true,
     node: true,
   },
-  extends: [
-    'airbnb',
-    'airbnb-typescript',
-    'airbnb/hooks',
-    'plugin:@typescript-eslint/recommended',
-    'eslint-config-prettier',
-  ],
+  extends: ['airbnb', 'airbnb-typescript', 'airbnb/hooks', 'plugin:@typescript-eslint/recommended', 'eslint-config-prettier', 'plugin:storybook/recommended'],
   parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaFeatures: {
--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,35 +1,18 @@
-import type { StorybookConfig } from '@storybook/react-webpack5';
+import type { StorybookConfig } from "@storybook/react-vite";
 
 const config: StorybookConfig = {
-  stories: ['../src/**/*.stories.@(js|jsx|ts|tsx)', '../src/stories/**/*.mdx'],
+  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
   addons: [
-    '@storybook/addon-links',
-    '@storybook/addon-essentials',
-    '@storybook/addon-styling',
-    '@storybook/addon-interactions',
+    "@storybook/addon-onboarding",
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@chromatic-com/storybook",
+    "@storybook/addon-interactions",
+    "@storybook/addon-themes"
   ],
   framework: {
-    name: '@storybook/react-webpack5',
+    name: "@storybook/react-vite",
     options: {},
   },
-  typescript: {
-    check: false,
-    checkOptions: {},
-    reactDocgen: 'react-docgen-typescript',
-    reactDocgenTypescriptOptions: {
-      // allowSyntheticDefaultImports: false, // speeds up storybook build time
-      // esModuleInterop: false, // speeds up storybook build time
-      shouldExtractLiteralValuesFromEnum: true, // makes union prop types like variant and size appear as select controls
-      shouldRemoveUndefinedFromOptional: true, // makes string and boolean types that can be undefined appear as inputs and switches
-      propFilter: (prop: any) =>
-        prop.parent
-          ? !/node_modules\/(?!@mui)/.test(prop.parent.fileName)
-          : true,
-    },
-  },
-  docs: {
-    autodocs: 'tag',
-  },
 };
-
 export default config;
